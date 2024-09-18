c3b9d1d1203f66f55f7cdd2c642e43c2a95738e7
valisoa01 <valisoa.rafanomezantsoa@student.passerellesnumeriques.org>
2024-09-18 07:59:39 +0300

    myfirst commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Bienvenue dans mon projet React !</h1>
+    </div>
+  );
+}
+
+ 
+
+export default App;
